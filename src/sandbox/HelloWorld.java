8938b1ab6142d525d50b0f6bf82ef7377b1f5986
eyalExperitest <eyal@experitest.com>
2017-01-24 11:31:17 +0200

    My Commits

---
+++ src/sandbox/HelloWorld.java
@@ -0,0 +1,16 @@
+package sandbox;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!!");
+		Thread.sleep(5000);
+		System.out.println("Hello World!!!!!");
+	}
+
+}
